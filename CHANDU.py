b5b047c231ba4eeb2f9699d272444b250a544076
chandupriyaj2003 <112478409+chandupriyaj2003@users.noreply.github.com>
2022-09-05 10:32:22 +0530

    Update CHANDU.py

--- CHANDU.py
+++ CHANDU.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.header('RAMACHANDRA COLLEGE OF ENGINEERING')
 a=st.number_input('ENTER ANY NUMBER')
-if st.button('CLICK HERE')
+if st.button('HIT ME')
 st.success(f'YOUR LUCKY NUMBER IS(a)')
